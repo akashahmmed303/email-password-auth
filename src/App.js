0139e2233baadf92763dd3699b9ea9a6e9ea2e0a
akashahmmed303 <ahmedakash336@gmail.com>
2022-10-21 05:54:15 +0600

    4

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import { getAuth } from 'firebase/auth';
-import app from './firebase/firebase.init';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Main from './components/Layout/Main';
+import LoginBootstrap from './components/LoginBootstrap';
 import RegisterReactBootstrap from './components/RegisterReactBootstrap';
 
-const auth = getAuth(app);
-
-const handleRegister = event => {
-  event.preventDefault();
-  const email = event.target.email.value;
-  const password = event.target.password.value;
-  console.log(email, password);
-};
-
-const handleEmailBlur = event => {
-  console.log(event.target.value);
-};
-
-const handlePasswordBlur = event => {
-  console.log(event.target.value);
-};
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>,
+      },
+      {
+        path: '/register',
+        element: <RegisterReactBootstrap></RegisterReactBootstrap>,
+      },
+      {
+        path: '/login',
+        element: <LoginBootstrap></LoginBootstrap>,
+      },
+    ],
+  },
+]);
 
 function App() {
   return (
     <div className=''>
-      <RegisterReactBootstrap></RegisterReactBootstrap>
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
---
+++ src/components/LoginBootstrap.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginBootstrap = () => {
+  return (
+    <div>
+      <h3 className='text-success'>Please Login !!</h3>
+    </div>
+  );
+};
+
+export default LoginBootstrap;

5feac906d989be6bbff540bb74b0696d1c20ccef
akashahmmed303 <ahmedakash336@gmail.com>
2022-10-22 04:12:11 +0600

    complate

--- src/components/RegisterReactBootstrap.js
+++ src/components/RegisterReactBootstrap.js
@@ -1,4 +1,9 @@
-import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  updateProfile,
+} from 'firebase/auth';
 import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -9,45 +14,83 @@ const auth = getAuth(app);
 
 const RegisterReactBootstrap = () => {
   const [passwordError, setPasswordError] = useState('');
+  const [success, setSuccess] = useState(false);
+
   const handleRegister = event => {
     event.preventDefault();
-    const email = event.target.email.value;
-    const password = event.target.password.value;
-    console.log(email, password);
+    setSuccess(false);
 
+    const form = event.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(name, email, password);
+
+    // validate password
     if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
       setPasswordError('Please provide at least two uppercase');
       return;
     }
     if (password.length < 6) {
-      setPasswordError('Please provide at least 6 characters');
+      setPasswordError('Please should be at least 6 characters.');
       return;
     }
-    if (!/(?=.*[!#$%&? "])/.test(password)) {
-      setPasswordError('Please add at least one special Character');
+    if (!/(?=.*[!@#$&*])/.test(password)) {
+      setPasswordError('Please add at least one special character');
       return;
     }
     setPasswordError('');
+
     createUserWithEmailAndPassword(auth, email, password)
       .then(result => {
         const user = result.user;
         console.log(user);
+        setSuccess(true);
+        form.reset();
+        verifyEmail();
+        updateUserName(name);
       })
       .catch(error => {
         console.error('error', error);
+        setPasswordError(error.message);
       });
+  };
+
+  const verifyEmail = () => {
+    sendEmailVerification(auth.currentUser).then(() => {
+      alert('Please check your email and verify your email address.');
+    });
+  };
+
+  const updateUserName = name => {
+    updateProfile(auth.currentUser, {
+      displayName: name,
+    })
+      .then(() => {
+        console.log('display name updated');
+      })
+      .catch(error => console.error(error));
   };
 
   return (
     <div className='w-50 mx-auto'>
-      <h3 className='text-primary'>Please Register</h3>
+      <h3 className='text-primary'>Please Register !!!</h3>
       <Form onSubmit={handleRegister}>
+        <Form.Group className='mb-3' controlId='formBasicName'>
+          <Form.Label>Your Name</Form.Label>
+          <Form.Control
+            type='text'
+            name='name'
+            placeholder='Enter Name'
+            required
+          />
+        </Form.Group>
         <Form.Group className='mb-3' controlId='formBasicEmail'>
           <Form.Label>Email address</Form.Label>
           <Form.Control
             type='email'
             name='email'
-            placeholder='Enter email '
+            placeholder='Enter email'
             required
           />
         </Form.Group>
@@ -61,16 +104,15 @@ const RegisterReactBootstrap = () => {
             required
           />
         </Form.Group>
-
         <p className='text-danger'>{passwordError}</p>
-
+        {success && <p className='text-success'>User Created Successfully.</p>}
         <Button variant='primary' type='submit'>
           Register
         </Button>
       </Form>
       <p>
         <small>
-          Already have an account? Please <Link to='/login'>Login</Link>
+          Already have an account? Please <Link to='/login'>Log in</Link>
         </small>
       </p>
     </div>
